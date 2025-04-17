58ae67f2d53334145f624a1b59fca20f1dbf684c
Space Octahedron <contact@spaceoctahedron.com>
2025-04-17 20:41:02 +0200

    Update the circle to the triangle pointer and optimize it for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,11 +25,10 @@ const Hero = () => {
       
       <Model3DCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute bottom-10 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex 
-          justify-center items-start p-2">
-            <motion.dev
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
               animate={{
                 y: [0, 24, 0],
               }}
@@ -37,8 +36,9 @@ const Hero = () => {
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
+                ease: "easeInOut",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-4 h-3 bg-[#8b76e9] clip-triangle rounded-lg"
             />
           </div>
         </a>
